c994363fd501cfb70ca10157402e365c54852709
traderon <chelizerb@gmail.com>
2024-09-29 08:55:23 -0500

    first commit

--- app.py
+++ app.py
@@ -840,8 +840,8 @@ def siigo_account_trimestral(user,token):
       monthStart=1
       monthEnd=1
       update=False
-      return jsonify({"success": True})
       "guardar la informacion trimestral por c/año en la tabla reports_filters"
+    return jsonify({"success": True})
 
 @app.route("/api/import_reports_siigo", methods=["POST"])
 def import_reports_siigo():
