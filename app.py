70ce95bd36040194b5b8315ef707edef735287d3
traderon <chelizerb@gmail.com>
2024-09-28 21:18:10 -0500

    first commit

--- app.py
+++ app.py
@@ -582,8 +582,8 @@ def siigo_account_trimestral(user,token):
                yearLastDate=yearLastDate+1
         update=True
     years=[2024]
-    monthStart=7
-    monthEnd=7
+    monthStart=6
+    monthEnd=6
     for year in years:
       saldo=[]
       ventas=[]
